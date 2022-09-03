c8a373c2ddbdb7c3ccb6e2687fb45add95e264f9
RicardoMenezesBandeira <ricardobandeira197@gmail.com>
2022-09-02 21:23:54 -0300

    alterações do joão

--- trab001/trabalho ED_1.c
+++ trab001/trabalho ED_1.c
@@ -1,79 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 typedef struct arvore{
     int info;
     struct arovre *esq;
     struct arvore *dir;
 }arvore;
-int menu(){
-    int opcao;
-    char nomeArq[20];
-    arvore *a = NULL;
-    FILE *arq = NULL;
-    printf("1- Ler uma árvore de um arquivo fornecido\n");
-    printf("2- Imprimir a árvore\n");
-    printf("3- Verificar se um elemento x existe na árvore\n");
-    printf("4- Contar o número de elemenetos na árvore\n");
-    printf("5- Imprimir os nós folhas da árvore\n");
-    printf("6- Sair\n");
-    scanf("%d",&opcao);
 
-    switch(opcao){
-        case 1:
-            printf("Digite o nome do arquivo: ");
-            scanf("%s",nomeArq);
-            arq = fopen(nomeArq,"rt");
-            a = LerArvore(&arq);
-            fclose(arq);
-            break;
-        case 2:
-            ImprimirArvore(a);
-            break;
-        case 3:
-
-            break;
-        case 4:
-
-            break;
-        case 5:
-
-            break;
-        case 6:
-            return;
-    }
-
-    menu();
-}
-arvore *ler(FILE *arq){
+arvore* LerArvore(FILE *arq,arvore *a){//codigo do Dalessandro pra criar a arvore a partir da notação ensinada em aula
     char c;
     int num;
-    arvore *a;
 
-    fscanf(arq, "%c",&c);//ler (
-    fscanf(arq, "%s",&num);//ler algarismo
-    if(num==-1){//-1 vale como null
+    fscanf(arq,"%c",&c);    //Ler '('
+    fscanf(arq,"%d",&num);  //Ler a raiz
+
+    if(num == -1){
+        fscanf(arq,"%c",&c);
         return NULL;
-    }else{
-        a = (arvore*)malloc(sizeof(arvore));//separa espaço da memoria para um elemento da arvore e quarda o ponteiro em 'a'
+    }
+    else{
+        arvore *a = (arvore*)malloc(sizeof(arvore));
         a->info = num;
-        a->esq= ler(arq);//cria ramo para a esquerda ate encontrar null
-        a->dir= ler(arq);//cria ramo para a direita ate encontrar null
-        fscanf(arq, "%c",&c);//ler )
-        return a;//retorna a arvore
+        a->esq = LerArvore(arq,a->esq);
+        a->dir = LerArvore(arq,a->dir);
+        fscanf(arq,"%c",&c);    //Ler ')'
+        return a;
     }
 }
 
-void imprimirPre(arvore *a){//pré-ordem 
+void ImprimirPre(arvore *a){//pré-ordem
     if(a != NULL){
-
-        ImprimirArvore(a->esq);
         printf("%d ",a->info);
-        ImprimirArvore(a->dir);
+        ImprimirPre(a->esq);
+        ImprimirPre(a->dir);
     }
-
 }
-void imprimirEm(arvore *a){}
-void imprimirPos(arvore *a){}
-void encontra(arvore *a,int x){}
+
+void ImprimirEmOrdem(arvore *a){//em-ordem
+    if(a != NULL){
+        ImprimirEmOrdem(a->esq);
+        printf("%d ",a->info);
+        ImprimirEmOrdem(a->dir);
+    }
+}
+
+void ImprimirPos(arvore *a){//pós-ordem
+    if(a != NULL){
+        ImprimirPos(a->esq);
+        ImprimirPos(a->dir);
+        printf("%d ",a->info);
+    }
+}
+
+int VerificaX(arvore *a, int x){//verifica se um elemento X está na arvore
+    if(a == NULL){// se entrar nesse if já passou por todos os ramos
+        return 0;
+    }
+    else if(a->info == x){// se entrar nesse if encontrou o elemento
+        return 1;
+    }
+    else{
+        return (VerificaX(a->esq,x) || VerificaX(a->dir,x));//retorna para a raiz a resposta da chamada dos nós
+    }
+}
 void contarElementos(arvore *a){}
 void imprimirFolhas(arvore *a){}
 arvore *destroi(arvore *a){//revisar
@@ -89,15 +78,68 @@ arvore *destroi(arvore *a){//revisar
         return NULL;
     }
 }
+
+void menu(arvore *raiz){//o ponteiro raiz é criado na main e é usado como argumento por todas as funções do menu
+//antes não funcionava pq a declaração do ponteiro era feita no proprio menu e sempre que ele era chamado recursivamente o valor era alterado pra NULL
+    int opcao,x;//opcao é a váriavel de escolha do menu e X é o elemento a ser procurado na arvore por uma das funções
+    char nomeArq[20];
+    FILE *arq = NULL;
+    printf("1- Ler uma árvore de um arquivo fornecido\n");
+    printf("2- Imprimir a árvore\n");
+    printf("3- Verificar se um elemento x existe na árvore\n");
+    printf("4- Contar o número de elementos na árvore\n");
+    printf("5- Imprimir os nós folhas da árvore\n");
+    printf("9- Sair\n");
+    scanf("%d",&opcao);
+    switch(opcao){
+        case 1:
+            printf("Digite o nome do arquivo: ");
+            scanf("%s",nomeArq);
+            arq = fopen(nomeArq,"rt");
+            raiz = LerArvore(arq,raiz);
+            fclose(arq);
+            break;
+        case 2:
+            printf("1- Pre-Ordem\n");
+            printf("2- Em-Ordem\n");
+            printf("3- Pos-Ordem\n");
+            printf("9- Voltar ao menu principal\n");
+            scanf("%d",&opcao);
+            switch(opcao){
+                case 1:
+                    ImprimirPre(raiz);
+                    printf("\n");
+                    break;
+                case 2:
+                    ImprimirEmOrdem(raiz);
+                    printf("\n");
+                    break;
+                case 3:
+                    ImprimirPos(raiz);
+                    printf("\n");
+                    break;
+                case 9:
+                    break;//volta ao menu principal
+            }
+            break;
+        case 3:
+            printf("Digite o elemento a ser procurado na árvore: ");
+            scanf("%d",&x);
+            printf("%d\n",VerificaX(raiz,x));
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 9:
+            return;//encerramento do programa caso o usuario digite 9
+    }
+    menu(raiz);
+}
 int main(){
-//    FILE *arq = NULL;
-//    char nomeArq[200];
-//    scanf("%s",nomeArq);
-//    arq = fopen(nomeArq,"rt");
-//    arvore *a = NULL;
-//    a = LerArvore(arq);
-//    ImprimirArvore(a);
-//    fclose(arq);
-    menu();
-return 0;
+    arvore *raiz = NULL;//criacao do primeiro nó da arvore que será passado por todas as funções
+    menu(raiz);//chamada da função menu implementada de forma recursiva
+    return 0;
 }
